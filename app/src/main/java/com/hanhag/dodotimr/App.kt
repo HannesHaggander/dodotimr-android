f8e145c799bb5690feef133ac1aea8554e6afc6f
Hannes <h.haggander@gmail.com>
2020-06-30 16:47:00 +0200

    init commit

---
+++ app/src/main/java/com/hanhag/dodotimr/App.kt
@@ -0,0 +1,11 @@
+package com.hanhag.dodotimr
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/com/hanhag/dodotimr/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hanhag.dodotimr.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hanhag.dodotimr.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
